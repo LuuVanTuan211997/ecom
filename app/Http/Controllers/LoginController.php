fe1d43aabfb2787f6ccd317c9a3328e12a27ccca
Lưu Văn Tuân <tuanlv2191997@gmail.com>
2023-11-08 16:26:04 +0700

    Khai bao login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    protected function getLogin()
+    {
+        dd(123);
+        return 1;
+    }
+}
